0594b8a23c73f5bda1f51e62eb526d2f7f36363d
Pietro Gagliardi <pietro10@mac.com>
2016-12-02 12:38:21 -0500

    Finished the C++11 tests.

--- benchmark/sys_cpp11.cpp
+++ benchmark/sys_cpp11.cpp
@@ -1,24 +1,31 @@
 // 30 november 2016
 #include <codecvt>
+#include <locale>
 #include <chrono>
-// change any definition to #define name NULL if it should not be defined
+#include <type_traits>
 extern "C" {
+#include <stdio.h>
 #include "common.h"
 }
 
-template<typename T> static int64_t nowToNsec(T now)
-{
-	typename T::duration d;
+// thanks to a lot of people in freenode ##c++ for help
 
-	d = now.time_since_epoch();
-	return std::chrono::nanoseconds(d).count();
-}
+// the high resolution clock does not need to be steady
+// this is a benchmark; prefer steadiness (TODO does Go?)
+// don't do this check at runtime every time
+using steadyClock = typename std::conditional<
+	std::chrono::high_resolution_clock::is_steady,
+	std::chrono::high_resolution_clock,
+	std::chrono::steady_clock>::type;
 
 int64_t benchCurrentTime(void)
 {
-	if (std::chrono::high_resolution_clock::is_steady)
-		return nowToNsec(std::chrono::high_resolution_clock::now());
-	return nowToNsec(std::chrono::steady_clock::now());
+	typename steadyClock::time_point now;
+	typename steadyClock::time_point::duration d;
+
+	now = steadyClock::now();
+	d = now.time_since_epoch();
+	return std::chrono::nanoseconds(d).count();
 }
 
 int64_t benchOneSecond(void)
@@ -31,13 +38,28 @@ int64_t benchTimeToNsec(int64_t c)
 	return c;		// already nanoseconds
 }
 
-static std::codecvt_utf8_utf16<char16_t> cvtUTF8UTF16;
-static std::codecvt_utf8<char32_t> cvtUTF8Rune;
-static std::codecvt_utf16<char32_t> cvtUTF16Rune;
+typedef std::codecvt_utf8_utf16<char16_t> typeUTF8UTF16;
+typedef std::codecvt_utf8<char32_t> typeUTF8Rune;
+typedef std::codecvt_utf16<char32_t> typeUTF16Rune;
+
+static typeUTF8UTF16 cvtUTF8UTF16;
+static typeUTF8Rune cvtUTF8Rune;
+static typeUTF16Rune cvtUTF16Rune;
+
+// TODO does this impact performance?
+static std::wstring_convert<typeUTF8UTF16, char16_t> wcvtUTF8UTF16;
+static std::wstring_convert<typeUTF8Rune, char32_t> wcvtUTF8Rune;
+
+static char32_t encodeASCIIUTF32[1] = { (char32_t) encodeASCII };
+static char32_t encodeJPUTF32[1] = { (char32_t) encodeJP };
+static char32_t encodeEmojiUTF32[1] = { (char32_t) encodeEmoji };
 
 void init(void)
 {
-	// code here
+	if (std::chrono::high_resolution_clock::is_steady)
+		printf("benchmarking with std::high_resolution_clock\n");
+	else
+		printf("benchmarking with std::steady_clock\n");
 }
 
 static void systemBenchmarkUTF8RuneCountASCIIBuf(int64_t n)
@@ -45,7 +67,7 @@ static void systemBenchmarkUTF8RuneCountASCIIBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8Rune.from_bytes(asciiBufUTF8).size();
 	}
 }
 
@@ -54,7 +76,7 @@ static void systemBenchmarkUTF8UTF16CountASCIIBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8UTF16.from_bytes(asciiBufUTF8).size();
 	}
 }
 
@@ -63,7 +85,7 @@ static void systemBenchmarkUTF8RuneCountJPBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8Rune.from_bytes(jpBufUTF8).size();
 	}
 }
 
@@ -72,7 +94,7 @@ static void systemBenchmarkUTF8UTF16CountJPBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8UTF16.from_bytes(jpBufUTF8).size();
 	}
 }
 
@@ -81,7 +103,7 @@ static void systemBenchmarkUTF8RuneCountEmojiBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8Rune.from_bytes(emojiBufUTF8).size();
 	}
 }
 
@@ -90,169 +112,260 @@ static void systemBenchmarkUTF8UTF16CountEmojiBuf(int64_t n)
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8UTF16.from_bytes(emojiBufUTF8).size();
 	}
 }
 
-static void systemBenchmarkUTF16RuneCountASCIIBuf(int64_t n)
-{
-	int64_t i;
-
-	for (i = 0; i < n; i++) {
-		// code here
-	}
-}
+#define systemBenchmarkUTF16RuneCountASCIIBuf NULL
 
 static void systemBenchmarkUTF16UTF8CountASCIIBuf(int64_t n)
 {
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// silence compiler about signedness
+		wcvtUTF8UTF16.to_bytes(reinterpret_cast<const char16_t *>(asciiBufUTF16)).size();
 	}
 }
 
-static void systemBenchmarkUTF16RuneCountJPBuf(int64_t n)
-{
-	int64_t i;
-
-	for (i = 0; i < n; i++) {
-		// code here
-	}
-}
+#define systemBenchmarkUTF16RuneCountJPBuf NULL
 
 static void systemBenchmarkUTF16UTF8CountJPBuf(int64_t n)
 {
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8UTF16.to_bytes(reinterpret_cast<const char16_t *>(jpBufUTF16)).size();
 	}
 }
 
-static void systemBenchmarkUTF16RuneCountEmojiBuf(int64_t n)
-{
-	int64_t i;
-
-	for (i = 0; i < n; i++) {
-		// code here
-	}
-}
+#define systemBenchmarkUTF16RuneCountEmojiBuf NULL
 
 static void systemBenchmarkUTF16UTF8CountEmojiBuf(int64_t n)
 {
 	int64_t i;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		wcvtUTF8UTF16.to_bytes(reinterpret_cast<const char16_t *>(emojiBufUTF16)).size();
 	}
 }
 
 static void systemBenchmarkUTF8EncodeRuneASCII(int64_t n)
 {
 	int64_t i;
+	char buf[4];
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.out(mb,
+			encodeASCIIUTF32, encodeASCIIUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF8EncodeRuneJP(int64_t n)
 {
 	int64_t i;
+	char buf[4];
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.out(mb,
+			encodeJPUTF32, encodeJPUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF8EncodeRuneEmoji(int64_t n)
 {
 	int64_t i;
+	char buf[4];
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.out(mb,
+			encodeEmojiUTF32, encodeEmojiUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16EncodeRuneASCII(int64_t n)
 {
 	int64_t i;
+	char buf[4];		// note: std::codecvt_utf16 stores UTF-16 in bytes :/
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.out(mb,
+			encodeASCIIUTF32, encodeASCIIUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16EncodeRuneJP(int64_t n)
 {
 	int64_t i;
+	char buf[4];
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.out(mb,
+			encodeJPUTF32, encodeJPUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16EncodeRuneEmoji(int64_t n)
 {
 	int64_t i;
+	char buf[4];
+	const char32_t *fromNext;
+	char *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.out(mb,
+			encodeEmojiUTF32, encodeEmojiUTF32 + 1, fromNext,
+			buf, buf + 4, toNext);
 	}
 }
 
 static void systemBenchmarkUTF8DecodeRuneASCII(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.in(mb,
+			asciiDecodeUTF8, asciiDecodeUTF8 + 1, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
 static void systemBenchmarkUTF8DecodeRuneJP(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.in(mb,
+			jpDecodeUTF8, jpDecodeUTF8 + 3, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
 static void systemBenchmarkUTF8DecodeRuneEmoji(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF8Rune.in(mb,
+			emojiDecodeUTF8, emojiDecodeUTF8 + 4, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16DecodeRuneASCII(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *from = reinterpret_cast<const char *>(asciiDecodeUTF16);
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.in(mb,
+			from, from + 2, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16DecodeRuneJP(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *from = reinterpret_cast<const char *>(jpDecodeUTF16);
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.in(mb,
+			from, from + 2, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
 static void systemBenchmarkUTF16DecodeRuneEmoji(int64_t n)
 {
 	int64_t i;
+	char32_t buf[1];
+	const char *from = reinterpret_cast<const char *>(emojiDecodeUTF16);
+	const char *fromNext;
+	char32_t *toNext;
 
 	for (i = 0; i < n; i++) {
-		// code here
+		// TODO discount state reset from benchmark?
+		std::mbstate_t mb = std::mbstate_t();
+
+		// TODO error check
+		cvtUTF16Rune.in(mb,
+			from, from + 4, fromNext,
+			buf, buf + 1, toNext);
 	}
 }
 
